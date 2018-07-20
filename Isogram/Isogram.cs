09fe864e4782e74cc8fc9b617fa7dbadb99fd385
Preety Kumari <preetydakshana@gmail.com>
2018-07-20 15:39:09 +0530

    solution updated

--- Isogram/Isogram.cs
+++ Isogram/Isogram.cs
@@ -1,12 +1,26 @@
 ﻿using System;
-
+using System.Linq;
 namespace Isogram
 {
     public class Isogram
     {
 		public static bool IsIsogram(string input) {
-			bool Flag = true;
-			
+			bool Flag = false;
+			String str = "";
+			for (int i=0;i<input.Length;i++) {
+				if (((int)input[i] >= (int)'a' && (int)input[i] <= (int)'z') ||
+					((int)input[i] >= (int)'A' && (int)input[i] <= (int)'Z'))
+				{
+					str += input[i];
+				}
+				
+			}
+			var DistinctChar = str.Distinct<char>();
+			if (DistinctChar.Count() == str.Length) {
+				Flag = true;
+
+			}
+
 			return Flag;
 		}
     }
