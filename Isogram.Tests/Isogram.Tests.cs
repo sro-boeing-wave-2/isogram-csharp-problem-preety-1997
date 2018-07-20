ccfeaa628465383d2c17d1841c5894d72996706b
Preety Kumari <preetydakshana@gmail.com>
2018-07-20 15:15:09 +0530

    test case updated

--- Isogram.Tests/Isogram.Tests.cs
+++ Isogram.Tests/Isogram.Tests.cs
@@ -1,14 +1,50 @@
 using System;
 using Xunit;
-
+using Isogram;
+using FluentAssertions;
 namespace Isogram.Tests
 {
-    public class IsogramTest
-	{
+    public class IsogramTests
+    {
         [Fact]
-        public void Test1()
+        public void TestForEmptyString()
         {
-
+			Isogram.IsIsogram("").Should().BeTrue();
         }
-    }
+		[Fact]
+		public void TestForIsogram() {
+			Isogram.IsIsogram("isogram").Should().BeTrue();
+		}
+		[Fact]
+		public void TestForEleven()
+		{
+			Isogram.IsIsogram("eleven").Should().BeFalse();
+		}
+		[Fact]
+		public void TestForSubdermatoglyphic()
+		{
+			Isogram.IsIsogram("subdermatoglyphic").Should().BeTrue();
+		}
+		[Fact]
+		public void TestForDuplicateCharInDifferentCase()
+		{
+			Isogram.IsIsogram("Alphabet").Should().BeTrue();
+		}
+		[Fact]
+		public void TestForStringWithHyphen()
+		{
+			Isogram.IsIsogram("thumbscrew-japingly").Should().BeTrue();
+		}
+		[Fact]
+		public void TestForStringWithDuplicateHyphen()
+		{
+			Isogram.IsIsogram("six-year-old").Should().BeTrue();
+		}
+		[Fact]
+		public void TestForStringWithSpaces()
+		{
+			Isogram.IsIsogram("thumbscrew japingly").Should().BeTrue();
+		}
+
+	}
 }
--- Isogram/Isogram.cs
+++ Isogram/Isogram.cs
@@ -4,5 +4,10 @@ namespace Isogram
 {
     public class Isogram
     {
+		public static bool IsIsogram(string input) {
+			bool Flag = true;
+			
+			return Flag;
+		}
     }
 }
